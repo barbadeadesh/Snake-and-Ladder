1ce4efff2b5a89737d95b81d2a260470904b4346
barbadeadesh <barbadeadesh2020@email.com>
2022-01-31 22:50:48 +0530

    get a number

---
+++ DiceRoll.java
@@ -0,0 +1,16 @@
+public class uc2_DiceRoll {
+
+	public static void main(String[] args) {
+		int Player1Position = 0;
+		System.out.println("Welcome to Snake and Ladder game");
+		int PlayerPosition = 0;
+		System.out.println("Game begins Player Position is: "+PlayerPosition);
+		DiceThrow();
+}
+		public static void DiceThrow(){
+			int Dice = (int)Math.floor(Math.random()*10)%6+1;
+			System.out.println("You got Dice Value "+Dice);
+
+	}
+
+}
